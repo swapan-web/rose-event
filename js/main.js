49ca5791ec6c271f6357ba58a587fb31070ba3b3
Swapan <swapan.exe@gmail.com>
2023-07-04 16:23:02 +0600

    Nav jump js moved to main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const navbar = document.getElementById("navbar");
+let scrolled = false;
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove("top");
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+      setTimeout(() => {
+        navbar.style.transform = "translateY(0)";
+        scrolled = true;
+      }, 200);
+    }
+  } else {
+    navbar.classList.add("top");
+    scrolled = false;
+  }
+};
+//<!-- smooth scroll -->
+$("#navbar a, .btn").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
